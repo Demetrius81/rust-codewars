6d16654366e839e2212a4e98374d71f4ce131ca3
Dzmitry Ryzhov <demetrius81ryzhov@gmail.com>
2025-08-13 09:03:20 +0300

    feat: add trebushet pro

--- src/sol/trebuchet_updatet.rs
+++ src/sol/trebuchet_updatet.rs
@@ -1,14 +1,10 @@
 #[allow(dead_code)]
 use rayon::prelude::*;
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-#[allow(dead_code)]
-
 pub fn run() {
     let rez = match read_file_to_vec("./calibration_data.txt") {
-        // Ok(x) => Some(get_first_last_digit(&create_digit_string(&x[2]))),
         Ok(x) => Some(get_vec_from_page(x).into_iter().sum::<i32>()),
         Err(_) => None,
     };
@@ -18,17 +14,10 @@ pub fn run() {
 
 fn get_vec_from_page(lines: Vec<String>) -> Vec<i32> {
     lines
-        // .into_par_iter()
-        .into_iter()
+        .into_par_iter()
+        // .into_iter()
         .filter_map(|s| {
             if let Some(num) = get_first_last_digit(&create_digit_string(&s)) {
-                _ = write_to_file(
-                    "output.txt",
-                    &create_digit_string(&s)
-                        .chars()
-                        .filter(|c| c.is_ascii_digit())
-                        .collect::<String>(),
-                );
                 Some(num)
             } else {
                 println!("No digits in line");
@@ -38,46 +27,46 @@ fn get_vec_from_page(lines: Vec<String>) -> Vec<i32> {
         .collect()
 }
 
-fn write_to_file(filename: &str, content: &str) -> io::Result<()> {
-    use std::fs::OpenOptions;
-    use std::io::Write;
+// fn write_to_file(filename: &str, content: &str) -> io::Result<()> {
+//     use std::fs::OpenOptions;
+//     use std::io::Write;
 
-    let mut file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(filename)?;
+//     let mut file = OpenOptions::new()
+//         .append(true)
+//         .create(true)
+//         .open(filename)?;
 
-    writeln!(file, "{}", content)?;
-    Ok(())
-}
+//     writeln!(file, "{}", content)?;
+//     Ok(())
+// }
 
 fn read_file_to_vec(filename: &str) -> io::Result<Vec<String>> {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
-
     let lines = reader.lines().collect::<Result<Vec<_>, _>>()?;
 
     Ok(lines)
 }
 
 fn create_digit_string(s: &str) -> String {
-    let mut map = HashMap::new();
-    map.insert("zero", 0);
-    map.insert("one", 1);
-    map.insert("two", 2);
-    map.insert("three", 3);
-    map.insert("four", 4);
-    map.insert("five", 5);
-    map.insert("six", 6);
-    map.insert("seven", 7);
-    map.insert("eight", 8);
-    map.insert("nine", 9);
+    let numbers = [
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
 
     let mut s = s.to_lowercase();
 
-    for (word, &num) in &map {
-        while s.contains(word) {
-            s = s.replace(word, &num.to_string());
+    for &(word, num) in &numbers {
+        while let Some(pos) = s.find(word) {
+            s.replace_range(pos..pos + word.len(), &num.to_string());
         }
     }
 
@@ -88,7 +77,6 @@ fn get_first_last_digit(s: &str) -> Option<i32> {
     let digits: Vec<char> = s.chars().filter(|c| c.is_ascii_digit()).collect();
 
     if digits.is_empty() {
-        println!("No digits in line");
         None
     } else {
         Some(
